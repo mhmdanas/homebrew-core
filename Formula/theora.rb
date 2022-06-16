8d66316453fdb9454d4fd616c6aa85f020e346ec
mhmdanas <triallax@tutanota.com>
2022-06-16 16:02:59 +0300

    theora: use smaller tarball

--- Formula/theora.rb
+++ Formula/theora.rb
@@ -4,9 +4,9 @@ class Theora < Formula
   license "BSD-3-Clause"
 
   stable do
-    url "https://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.bz2", using: :homebrew_curl
-    mirror "https://ftp.osuosl.org/pub/xiph/releases/theora/libtheora-1.1.1.tar.bz2"
-    sha256 "b6ae1ee2fa3d42ac489287d3ec34c5885730b1296f0801ae577a35193d3affbc"
+    url "https://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.xz", using: :homebrew_curl
+    mirror "https://ftp.osuosl.org/pub/xiph/releases/theora/libtheora-1.1.1.tar.xz"
+    sha256 "f36da409947aa2b3dcc6af0a8c2e3144bc19db2ed547d64e9171c59c66561c61"
 
     # Fix -flat_namespace being used on Big Sur and later.
     patch do
